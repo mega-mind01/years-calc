756fdb42f13d8a2d9badf3629fedd0f86455e9a6
Timothy Meleki <muyiwameleki@gmail.com>
2023-09-19 08:00:54 +0100

    new commit

--- script.js
+++ script.js
@@ -2,39 +2,24 @@ let check = document.getElementById("check")
 
 check.addEventListener("click", checker);
 
-// alert("oya na senior man");
-
 function checker() {
-    let age_y = parseInt(document.getElementById("yrs").value);
-    let age_w = parseInt(document.getElementById("wks").value);
-    let age_d = parseInt(document.getElementById("days").value);
+    let age = parseInt(document.getElementById("yrs").value);
 
-    // year of interest in days
-    const TARGET_YRS = 90 * 365;
-    const AVG_DAYS_IN_MONTH = 30.4;
-    const AVG_DAYS_IN_WEEK = 7;
+    const TARGET_YRS = 90;
+    const DAYS_IN_YEAR = 365;
+    const DAYS_IN_MONTH = 30.4;
+    const DAYS_IN_WEEK = 7;
 
-    if (isNaN(age_w) || isNaN(age_d)) {
-        let age = 90 - age_y;
-        age = age * 365;
-        let a_months = parseInt(age / AVG_DAYS_IN_MONTH);
-        let a_week = parseInt((age - (a_months * AVG_DAYS_IN_MONTH)) / AVG_DAYS_IN_WEEK);
-        let a_day = parseInt(age - (a_months * AVG_DAYS_IN_MONTH) - (a_week * AVG_DAYS_IN_WEEK));
-        document.getElementById("ageDisplay").innerHTML = "You have " + a_months + " months, " + a_week + " weeks, and " + a_day + " days left until you turn 90 years old";
+    if (age < 0) {
+        document.getElementById("ageDisplay").innerHTML = "Shey you dey whine me?";
+    } else if (age >= TARGET_YRS) {
+        document.getElementById("ageDisplay").innerHTML = "You have already reached 90 years!";
     } else {
-        age_y = age_y * 365;
-        age_w = age_w * 7;
-        let age_in_days = age_y + age_w + age_d;
-        let days_left = TARGET_YRS - age_in_days;
+        let yearsLeft = TARGET_YRS - age;
+        let daysLeft = yearsLeft * DAYS_IN_YEAR;
+        let monthsLeft = parseInt(daysLeft / DAYS_IN_MONTH);
+        let weeksLeft = parseInt(daysLeft / DAYS_IN_WEEK);
 
-        let in_month = parseInt(days_left / AVG_DAYS_IN_MONTH);
-        let in_week = parseInt((days_left - (in_month * AVG_DAYS_IN_MONTH)) / AVG_DAYS_IN_WEEK);
-        let in_day = parseInt(days_left - (in_month * AVG_DAYS_IN_MONTH) - (in_week * AVG_DAYS_IN_WEEK));
-
-        if (days_left < 0) {
-            document.getElementById("ageDisplay").innerHTML = "Shey you dey whine me?";
-        } else {
-            document.getElementById("ageDisplay").innerHTML = "You have " + in_month + " months, " + in_week + " weeks, and " + in_day + " days left until you turn 90 years old";
-        }
+        document.getElementById("ageDisplay").innerHTML = "You have " + monthsLeft + " months, " + weeksLeft + " weeks, and " + daysLeft + " days left until you turn 90 years old";
     }
 }
